a59e0181bbe1a19db3d69323cddb0b5f9c589723
CarsonDecker24 <carson.decker@stu.daviess.kyschools.us>
2023-10-19 12:18:58 -0500

    remove arrows on enemy death

--- bullet.gd
+++ bullet.gd
@@ -9,30 +9,22 @@ var has_been_attached = false
 var on_enemy = false
 var current_attached_body: Node
 var offset = Vector2(0,0)
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _process(delta): 
-	if grabbed_og == false:
-		compared_to_previous =  (og_y_position-global_position.y)-vertical_change
+	#Make arrow dip down after firing
+	#if grabbed_og == false:
+	#	compared_to_previous =  (og_y_position-global_position.y)-vertical_change
 	
-	if grabbed_og == true:
-		grabbed_og = false
-		og_y_position = global_position.y
-	vertical_change = og_y_position-global_position.y
-	
-	#print(compared_to_previous)
-	
-	if is_attached:
-		has_been_attached = true
+	#if grabbed_og == true:
+	#	grabbed_og = false
+	#	og_y_position = global_position.y
+	#vertical_change = og_y_position-global_position.y
 	
 	if on_enemy:
 		if current_attached_body == null:
 			_detach()
 		else:
 			_keep_attached(current_attached_body)
-	
-	if has_been_attached and not is_attached:
-		linear_velocity.y += gravity * delta
 
 func _on_body_entered(body):
 	if body.is_in_group("Ground"):
@@ -67,5 +59,4 @@ func _keep_attached(body):
 	pass
 
 func _detach():
-	sleeping = false
-	is_attached = false
+	queue_free()
