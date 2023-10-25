6626ef4357621f8fbbed21422c5db3fe22417405
StephenBender2024 <142928247+StephenBender2024@users.noreply.github.com>
2023-10-25 13:07:15 -0500

    waterfall freezes 10/25/23

--- Water.gd
+++ Water.gd
@@ -1,8 +1,9 @@
 extends Node2D
-
+var frameHolder
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_node("StaticBody2D/CollisionShape2D").set_deferred("disabled", true)
 	pass # Replace with function body.
 
 
@@ -12,7 +13,32 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	#if body.has_group("Arrow"):
-	#	if body.type == "Ice":
-	#		get_node("StaticBody2D/CollisionShape2D").disabled = false
-	print("this hella broken, dog")
+	print("bad")
+	if body.is_in_group("Arrow"):
+		if body.type == "Ice":
+			get_node("StaticBody2D/CollisionShape2D").set_deferred("disabled", false)
+			print("did that")
+			_freeze()
+
+func _freeze():
+	frameHolder = get_node("p1").frame
+	get_node("p1").play("frozen_rock")
+	get_node("p1").set_frame_and_progress(frameHolder,0.00)
+	frameHolder = get_node("p1/p2").frame
+	get_node("p1/p2").play("frozen")
+	get_node("p1/p2").set_frame_and_progress(frameHolder,0.00)
+	frameHolder = get_node("p1/p2/p3").frame
+	get_node("p1/p2/p3").play("frozen")
+	get_node("p1/p2/p3").set_frame_and_progress(frameHolder,0.00)
+	frameHolder = get_node("p1/p2/p3/p4").frame
+	get_node("p1/p2/p3/p4").play("frozen")
+	get_node("p1/p2/p3/p4").set_frame_and_progress(frameHolder,0.00)
+	frameHolder = get_node("p1/p2/p3/p4/p5").frame
+	get_node("p1/p2/p3/p4/p5").play("frozen")
+	get_node("p1/p2/p3/p4/p5").set_frame_and_progress(frameHolder,0.00)
+	frameHolder = get_node("p1/p2/p3/p4/p5/p6").frame
+	get_node("p1/p2/p3/p4/p5/p6").play("frozen")
+	get_node("p1/p2/p3/p4/p5/p6").set_frame_and_progress(frameHolder,0.00)
+	frameHolder = get_node("p1/p2/p3/p4/p5/p6/p7").frame
+	get_node("p1/p2/p3/p4/p5/p6/p7").play("frozen")
+	get_node("p1/p2/p3/p4/p5/p6/p7").set_frame_and_progress(frameHolder,0.00)
