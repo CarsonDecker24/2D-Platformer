f7f6376ec593a6c3785739ee3ff15e022fc8e994
CarsonDecker24 <carson.decker@stu.daviess.kyschools.us>
2023-11-30 12:52:26 -0600

    11/30/23 stemen birthday

--- Wire.gd
+++ Wire.gd
@@ -1,4 +1,4 @@
-extends AnimatedSprite2D
+extends Node2D
 
 var on = false
 @onready var source = get_node(get_meta("Source"))
@@ -12,7 +12,7 @@ func _ready():
 func _process(delta):
 	if source.on:
 		on = true
-		get_node("wireOff/wireOn").show_behind_parent= true
+		get_node("wireOff/wireOn").show_behind_parent= false
 	else:
 		on = false
-		get_node("wireOff/wireOn").show_behind_parent= false
+		get_node("wireOff/wireOn").show_behind_parent= true
--- electric_door.gd
+++ electric_door.gd
@@ -13,7 +13,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if source.on:
+	if source != null and source.on:
 		if playing==true and $AnimatedSprite2D.is_playing()==false:
 			on = true
 			$AnimatedSprite2D.play("open")
