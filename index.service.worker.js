836dc3e82c8f6f845494143039cff025920e8417
CarsonDecker24 <carson.decker@stu.daviess.kyschools.us>
2024-02-06 13:14:22 -0600

    update againnnnn

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1707246546|3494649484";
+const CACHE_VERSION = "1707246848|3796772124";
 const CACHE_PREFIX = "2D-Platformer-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
